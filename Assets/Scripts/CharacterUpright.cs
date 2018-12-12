635965c3a960cdf3d2fb3fe9bb3dd7bc1f7a2da7
Jonek2208 <jonatanhrynko@gmail.com>
2018-12-12 04:01:28 +0100

    ragdoll created

---
+++ Assets/Scripts/CharacterUpright.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUpright : MonoBehaviour 
+{
+
+	new protected Rigidbody rigidbody;
+	public bool keepUpright = true;
+	public float uprightForce = 10;
+	public float uprightOffset = 1.45f;
+	public float additionalUpwardForce = 10;
+	public float dampenAngularForce = 0;
+
+	void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+		rigidbody.maxAngularVelocity = 40;
+	}
+
+	void FixedUpdate()
+	{
+		if(keepUpright)
+		{
+			rigidbody.AddForceAtPosition(new Vector3(0, uprightForce + additionalUpwardForce, 0), transform.position + transform.TransformPoint(new Vector3(0, uprightOffset, 0)), ForceMode.Force);
+			rigidbody.AddForceAtPosition(new Vector3(0, -uprightForce, 0), transform.position + transform.TransformPoint(new Vector3(0, -uprightOffset, 0)), ForceMode.Force);
+		}
+		if(dampenAngularForce > 0)
+		{
+			rigidbody.angularVelocity *= (1- Time.deltaTime * dampenAngularForce);
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+}
