2e13ccfc0b160194bd31e279cc778c73ceacab2c
jhrynko <jonatanhrynko@gmail.com>
2019-09-23 17:53:49 +0200

    [UI] added classes and modified calendar creator scene

---
+++ Assets/Scripts/CalendarCreatorScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+using CompetitionClasses;
+
+public class CalendarCreatorScript : MonoBehaviour
+{
+    const string competitorsFileName = "competitors.json";
+    public GameObject jumperPrefab;
+    public TMPro.TMP_InputField lastNameInput;
+    public TMPro.TMP_InputField firstNameInput;
+    public TMPro.TMP_InputField countryInput;
+    public TMPro.TMP_Dropdown genderDropdown;
+
+    List<Competitor> allCompetitors;
+
+    public void CreateList()
+    {
+        string filePath = Path.Combine(Application.streamingAssetsPath, competitorsFileName);
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            allCompetitors = JsonConvert.DeserializeObject<List<Competitor>>(dataAsJson);
+            foreach (var c in allCompetitors)
+            {
+                Debug.Log(c.firstName + " " + c.lastName);
+            }
+        }
+    }
+
+    void Start()
+    {
+        CreateList();
+    }
+}
---
+++ Assets/Scripts/CompetitionClasses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CompetitionClasses
+{
+    enum Gender { Male, Female }
+    enum LimitType { None, Normal, Exact }
+    enum RoundType { Normal, KO }
+    enum EventType { Qualification, Individual, Team }
+    enum ClassificationType { IndividualPlace, IndividualPoints, TeamPlace, TeamPoints }
+    enum RankType { None, Event, Classification }
+
+    class Competitor
+    {
+        public string lastName;
+        public string firstName;
+        public string countryCode;
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Gender gender;
+        public DateTime birthdate;
+        public Competitor() { }
+        public Competitor(string _lastName, string _firstName, string _countryCode, Gender _gender, int _year, int _month, int _day)
+        {
+            lastName = _lastName;
+            firstName = _firstName;
+            countryCode = _countryCode;
+            gender = _gender;
+            birthdate = new DateTime(_year, _month, _day);
+        }
+
+
+    }
+}
