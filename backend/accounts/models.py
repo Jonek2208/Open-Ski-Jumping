885143b0a403c1f998600af639e0bee32a0ffef9
Szymon Cader <szymon.SC.cader@gmail.com>
2019-10-29 23:10:58 +0100

    Add Score model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from skijump.models import TimestampModel, SkiJump
+
 
 class CustomUser(AbstractUser):
     pass
 
 
+class Score(TimestampModel):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    skijump = models.ForeignKey(SkiJump, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
